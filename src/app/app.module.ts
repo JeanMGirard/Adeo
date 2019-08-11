33010c9da5a3037740dc6b19d91b6468c51bbf5b
Jm <Jean.M.Girard@Outlook.com>
2019-08-11 04:52:22 -0400

    dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule,
     AppRoutingModule
   ],
   providers: [],
